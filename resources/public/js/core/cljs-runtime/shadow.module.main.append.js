181baac125c59feecd3774b42e2d24b317fb83b0
p4ulcristian <p4ulcristian@gmail.com>
2026-04-27 10:38:07 +0000

    chore: fix gitignore, untrack compiled js and runtime files

--- resources/public/js/core/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { manas.frontend.app.start_app_BANG_(); } catch (e) { console.error("An error occurred when calling (manas.frontend.app/start-app!)"); console.error(e); }
